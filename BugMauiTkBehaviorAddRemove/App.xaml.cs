558eb0783dfe23c20d7e5e2a6370e43934a9567f
HOMEPC\alex3696 <alex3696@yandex.ru>
2024-11-12 00:29:01 +0700

    first pull

---
+++ BugMauiTkBehaviorAddRemove/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace BugMauiTkBehaviorAddRemove
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ BugMauiTkBehaviorAddRemove/MainPage.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+
+namespace BugMauiTkBehaviorAddRemove
+{
+    public class BaseVM : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+        public void ChangeNotify([CallerMemberName] string? prop = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
+        protected bool SetProperty<T>(ref T field, T newValue, [CallerMemberName] string? propertyName = null)
+        {
+            if (!Equals(field, newValue))
+            {
+                field = newValue;
+                ChangeNotify(propertyName);
+                return true;
+            }
+            return false;
+        }
+    }
+    public class CollectionVM : BaseVM
+    {
+        public string? Pressed
+        {
+            get => _pressed;
+            set => SetProperty(ref _pressed, value);
+        }
+        public ObservableCollection<ItemVM> Items { get; } = [];
+        public ICommand CmdRefresh { get; }
+
+        public CollectionVM()
+        {
+            CmdRefresh = new Command(OnRefresh);
+        }
+        string? _pressed;
+        void OnRefresh()
+        {
+            Items.Clear();
+            for (int i = 0; i < 3; i++)
+                Items.Add(new ItemVM(this));
+        }
+    }
+
+    public class ItemVM : BaseVM
+    {
+        public string Data { get; }
+        public Command CmdPress { get; }
+        public ItemVM(CollectionVM owner)
+        {
+            _owner = owner;
+            Data = _data.ToString();
+            CmdPress = new Command(OnPress);
+            _data++;
+        }
+        CollectionVM _owner;
+        static int _data = 0;
+        void OnPress()
+        {
+            _owner.Pressed = Data;
+        }
+    }
+
+    public partial class MainPage : ContentPage
+    {
+        CollectionVM _vm;
+        public MainPage()
+        {
+            InitializeComponent();
+            _vm = new CollectionVM();
+            BindingContext = _vm;
+        }
+    }
+
+}
